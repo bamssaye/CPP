3362d2e418bd188d66b7daedb6abe7637184a8b6
bamssaye <bamssayef@gmail.com>
2024-06-09 23:51:06 +0100

    add

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,7 +6,24 @@
 /*   By: bamssaye <bamssaye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 23:01:43 by bamssaye          #+#    #+#             */
-/*   Updated: 2024/06/06 23:01:44 by bamssaye         ###   ########.fr       */
+/*   Updated: 2024/06/09 03:48:15 by bamssaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PhoneBook.h"
+
+class PhoneBook
+{
+  public:
+    int b;
+    char c;  
+};
+
+int main()
+{
+    std::string Name;
+
+    Name = "hello world";
+    
+    std::cout << Name << std::endl;
+}
--- ex01/PhoneBook.h
+++ ex01/PhoneBook.h
@@ -6,7 +6,33 @@
 /*   By: bamssaye <bamssaye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 23:01:45 by bamssaye          #+#    #+#             */
-/*   Updated: 2024/06/06 23:01:46 by bamssaye         ###   ########.fr       */
+/*   Updated: 2024/06/09 05:27:38 by bamssaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHONE_BOOK_H
+#define PHONE_BOOK_H
+
+#include <iostream>
+#include <string.h>
+
+
+class PhoneBook
+{
+    public:
+        char contact[8];
+    private:  
+};
+
+class Contact
+{
+  public:
+    std::string FirstName;
+    std::string LastName;
+    std::string NickName;    
+  private:    
+};
+
+
+
+#endif
