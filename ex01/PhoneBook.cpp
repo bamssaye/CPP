67a48554cb5d2156049b7ddeba0502e677a2e31c
bamssaye <bamssayef@gmail.com>
2024-06-12 17:59:47 +0100

    start

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: bamssaye <bamssaye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 23:01:43 by bamssaye          #+#    #+#             */
-/*   Updated: 2024/06/09 03:48:15 by bamssaye         ###   ########.fr       */
+/*   Updated: 2024/06/12 17:59:28 by bamssaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,36 @@
 class PhoneBook
 {
   public:
-    int b;
-    char c;  
+    std::string contact[8];
+
+};
+class Contact
+{
+  public:
+  
+  int _checkinput(std::string s1, std::string s2, std::string s3, std::string s4)
+  {
+    if (s1 == "" || s2 == "" || s3 == "" || s4 == "")
+      return (1);
+    return (0);
+  }
+  void _addcontact(){}
+  void _searchcontact(){}
+  void _exit(){}
+  private:
+    std::string FName;
+    std::string LName;
+    std::string NName;
+    std::string PNumber;
+    std::string DSecret;
 };
 
 int main()
 {
-    std::string Name;
+   
+    PhoneBook con1;
 
-    Name = "hello world";
-    
-    std::cout << Name << std::endl;
+    con1.contact
 }
+
+
